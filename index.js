af5fea89d0e81d1044ff0391aae97b6a33528273
Manoj Kumawat <mjay@Manojs-MacBook-Pro.local>
2023-06-06 21:39:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+app.get("/", function (req,res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function (req,res) {
+    const age = req.body.age;
+
+    res.send("age is : " + age);
+})
+
+
+app.listen(3000, function () {
+  console.log("Server is runnig  at Port 3000.");
+})
